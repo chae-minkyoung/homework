bb650a27ee96ed5d8262c89e233bbc3e8f5fc620
chae minkyoung <chea220215@gmail.com>
2023-01-16 17:30:51 +0900

    5-6

--- 5-4~8.py
+++ 5-4~8.py
@@ -1,21 +1,12 @@
  # .format('salutation', 'name', 'product','verbed','room', 'animals', 'amount', 'product','pecent','verbed','spokesman','job_title'))
 
 
-print("Dear {salutation} {name},".format(salutation = 'abc', name= 'minkyoung'))
-
-letter="""Dear {salutation} {name},
-Thank you for your letter. We are sorry that our {product}
-{verbed} in your {room}. Please note that it should never
-be used in a {room}, especially near any {animals}.
-Send us your receipt and {amount} for shipping and handling.
-We will send you another {product} that, in our tests,
-is {percent}% less likely to have {verbed}.
-Thank you for your support.
-Sincerely,
-{spokesman}
-{job_title}"""
-print(letter.format(salutation = 'a', name = 'b', product = 'c', verbed = 'd', room = 'e', animals = 'f', amount = 'g',spokesman = 'h',job_title = 'i', percent = 'j'))
-
+a='Ducky'
+b='Gourdface'
+c='Spitzy'
+print('an English submarine 이름은 (%s Mc%sface)로 지어졌다.'%(a,a))
+print('an Australian racehorse 이름은 (%s Mc%sface)로 지어졌다.'%(b,b))
+print('an Swedish train 이름은 (%s Mc%sface)로 지어졌다.'%(c,c))
 
 
 
